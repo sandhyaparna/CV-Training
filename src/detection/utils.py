f9588944184fc46dd62ee881c7f87347e260b793
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2025-11-21 11:32:04 -0600

    feat: training notebook for detection

--- src/detection/utils.py
+++ src/detection/utils.py
@@ -308,3 +308,17 @@ def init_distributed_mode(args):
                                          world_size=args.world_size, rank=args.rank)
     torch.distributed.barrier()
     setup_for_distributed(args.rank == 0)
+
+
+# Open log file and redirect stdout to both console and file
+class Tee:
+    """Write output to both console and file."""
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush()
+    def flush(self):
+        for f in self.files:
+            f.flush()
